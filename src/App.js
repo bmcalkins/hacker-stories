59a937c8c566313ea3589b00fe9ec104cacdd349
Bmcalkins <b.raders1013@gmail.com>
2021-03-27 16:45:45 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const title = 'React';
+
 function App() {
-  const title = 'React';
-  
+
   return (
     <div className="App">
       <h1>Hello World</h1>
