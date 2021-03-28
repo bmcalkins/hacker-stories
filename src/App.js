bcc7e7f8509fa3cbe46a216531aff6844faf545e
Bmcalkins <b.raders1013@gmail.com>
2021-03-27 22:02:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React'
-};
+function getTitle(title) {
+  return title;
+}
 
 function App() {
 
   return (
     <div className="App">
-      <h1> {welcome.greeting} {welcome.title} </h1>
+      <h1> hello {getTitle('React')} </h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
