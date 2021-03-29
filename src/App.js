4a960d137fedcb3a41efbc45f11cbc7544fa1163
Bmcalkins <b.raders1013@gmail.com>
2021-03-28 22:37:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+title: 'React',
+url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+points: 4,
+objectID: 0,
+}, {
+title: 'Redux',
+url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+points: 5,
+objectID: 1,
+}, ];
 
 function App() {
 
   return (
     <div className="App">
-      <h1> hello {getTitle('React')} </h1>
+      <h1> My Hacker Stories  </h1>
+
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+        {list.map(function(item) { return <div>{item.title}</div>;
+      })}
 
     </div>
   );
