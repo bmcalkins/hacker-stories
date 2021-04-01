3ca48df2f22a5e089ceb44a96c279f1799c3aab6
Bmcalkins <b.raders1013@gmail.com>
2021-04-01 15:06:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 const list = [
@@ -31,20 +31,19 @@ function App() {
   );
 }
 
-function list() {
-  return {
-    list.map(function (item) {
-      return (
-        (<div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-        );
-    })
-  }
+function List() {
+  return list.map(function(item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
+}
 
   export default App;
