d0f39bd0f33615e1024d1f29ab776476479e3ff2
Bmcalkins <b.raders1013@gmail.com>
2021-04-05 12:34:01 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,19 +31,19 @@ function App() {
   );
 }
 
-function List() {
-  return list.map(function(item) {
-    return (
-      <div key={item.objectID}>
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
         </span>
         <span>{item.author}</span>
         <span>{item.num_comments}</span>
         <span>{item.points}</span>
-      </div>
-    );
-  });
-}
+      </li>
+    ))}
+  </ul>
+);
 
   export default App;
